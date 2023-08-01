d15e5447a276cc444c1121a0cda0decd06a9bd3d
Diexvel <velardesdiego@gmail.com>
2023-08-01 18:16:01 -0300

    Se realiazo validaciones para el campo nombre

--- Controllers/TiposCuentasController.cs
+++ Controllers/TiposCuentasController.cs
@@ -5,6 +5,14 @@ namespace ManejoPresupuesto.Controllers
 {
 	public class TiposCuentasController : Controller
 	{
+
+		private readonly string connectionString;
+
+		public TiposCuentasController(IConfiguration configuration)
+		{
+			connectionString = configuration.GetConnectionString("DefaultConnection");
+		}
+		
 		public IActionResult Crear()
 		{
 			return View();
@@ -13,6 +21,11 @@ namespace ManejoPresupuesto.Controllers
 		[HttpPost]
 		public IActionResult Crear(TipoCuenta tipoCuenta)
 		{
+			if (!ModelState.IsValid)
+			{
+				return View(tipoCuenta);
+			}
+
 			return View();
 		}
 	}
